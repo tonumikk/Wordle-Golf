1cd9e5d8a19b0a168094c88e2c497ed02626cb38
Tonu Mikk <tmikk@umn.edu>
2023-05-18 15:59:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function appendItemTowordleSolutionEl(item,itemIDInDB) {
     
     newEl.textContent = itemValue
     
-    newEl.addEventListener("click", function() {
+    newEl.addEventListener("dblclick", function() {
         let exactLocationOfItemInDB = ref(database, `wordleSolution/${itemIDInDB}`)
         
         remove(exactLocationOfItemInDB)
